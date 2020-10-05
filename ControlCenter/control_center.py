90cc1133bc7f1da05b389b12be6b37a47deabef4
Calmtea <willy5209593@gmail.com>
2020-10-05 16:42:14 +0800

    When recrive a alert send email to user

--- ControlCenter/control_center.py
+++ ControlCenter/control_center.py
@@ -1,10 +1,31 @@
 import serial
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+def sendEmailAlert(msg):
+    with smtplib.SMTP(host="smtp.nlhs.tyc.edu.tw",port="25") as smtp:
+        content=MIMEMultipart()
+        content["subject"]="[警報]"+msg
+        content["from"]="s810088@email.nlhs.tyc.edu.tw"
+        content["to"]="s810459@email.nlhs.tyc.edu.tw"
+        content.attach(MIMEText(msg))
+        try:
+            smtp.ehlo()
+            smtp.send_message(content)
+            print("訊息已發送")
+        except Exception as e:
+            print("Error message:", e)
 S=serial.Serial("com5",9600,timeout=2)
+IntruderAlert=False
 while True:
     data=S.readline().decode().rstrip()
-    if data=="alert":
+    if data=="alert" and not IntruderAlert:
+        IntruderAlert=True
         print("入侵警報!!")
-    elif data=="normal":
+        sendEmailAlert("123456789")
+    elif data=="normal" and IntruderAlert:
+        IntruderAlert=False
         print("警報解除")
+        sendEmailAlert("警報解除")
     time.sleep(1)
