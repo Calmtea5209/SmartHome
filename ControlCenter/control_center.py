ae28756908eadcc37a350d8149f74367996567a3
Calmtea <willy5209593@gmail.com>
2020-10-05 15:31:45 +0800

    增加以vscode連接

---
+++ ControlCenter/control_center.py
@@ -0,0 +1,10 @@
+import serial
+import time
+S=serial.Serial("com5",9600,timeout=2)
+while True:
+    data=S.readline().decode().rstrip()
+    if data=="alert":
+        print("入侵警報!!")
+    elif data=="normal":
+        print("警報解除")
+    time.sleep(1)
